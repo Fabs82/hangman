2b6b9abd9adcac359485e3496dd8558b6f93df90
Fabs82 <148846635+Fabs82@users.noreply.github.com>
2025-10-30 12:51:44 +0100

    feat: add error handling when loading files

--- lib/file_manager.rb
+++ lib/file_manager.rb
@@ -4,11 +4,11 @@ require "json"
 class FileManager
   def initialize(filename)
     @filename = filename
-    @file_path = "lib/save/#{@filename}.json"
+    @file_path = File.join(__dir__, "save", "#{@filename}.json")
   end
 
   def save_game(data)
-    json_data = JSON.generate(data)
+    json_data = JSON.pretty_generate(data)
     File.open(@file_path, "w") do |file|
       file.write(json_data)
     end
@@ -18,5 +18,11 @@ class FileManager
   def load_game
     load_data = File.read(@file_path)
     JSON.parse(load_data)
+  rescue Errno::ENOENT
+    puts "Save file not found: #{@file_path}"
+    nil
+  rescue JSON::ParserError
+    puts "Save file corrupted: #{@file_path}"
+    nil
   end
 end
